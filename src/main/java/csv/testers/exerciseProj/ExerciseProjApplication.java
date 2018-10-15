7634943adcb5602f79bf72776ef67f09d7f8ad0e
Rafal Kepinski <kepinski85@gmail.com>
2018-10-15 13:32:28 +0200

    Commit to create

---
+++ src/main/java/csv/testers/exerciseProj/ExerciseProjApplication.java
@@ -0,0 +1,12 @@
+package csv.testers.exerciseProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciseProjApplication.class, args);
+	}
+}
---
+++ src/test/java/csv/testers/exerciseProj/ExerciseProjApplicationTests.java
@@ -0,0 +1,16 @@
+package csv.testers.exerciseProj;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExerciseProjApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
